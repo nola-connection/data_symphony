1c025a810c6f7e425f56d265faf67165d269e0a1
Matt Aguiluz <81602509+504aguiluz@users.noreply.github.com>
2026-06-22 17:03:53 +0200

    Cleans Unnecessary Comments

--- lib/data_symphony/application.ex
+++ lib/data_symphony/application.ex
@@ -1,6 +1,4 @@
 defmodule DataSymphony.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -18,8 +16,6 @@ defmodule DataSymphony.Application do
       DataSymphonyWeb.Endpoint
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: DataSymphony.Supervisor]
     Supervisor.start_link(children, opts)
   end
